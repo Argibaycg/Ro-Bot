639903fbfce4b6a0883b438fa3bbe4f9dc3bcf0e
Argibay Carlos Gaston <47309230+Argibaycg@users.noreply.github.com>
2021-12-08 20:28:29 +0000

    add if to recognize bot command on message

--- main.py
+++ main.py
@@ -15,22 +15,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	message_bot = 'No te entendi'
 	if message.author == client.user:
 		return
 
-	if message.content.startswith('-$'):
-		message_bot = get_crypto_value(message.content[2:])
-	if message.content.startswith('-meme'):
-		message_bot = get_meme()
-	if message.content.startswith('-noticia'):
-		message_bot = message.content.split('-noticia ', 1)[1]
-		message_bot = get_news(message_bot)
-	if message.content.startswith('-falopa'):
-		message_bot = 'Ahhhhh otra vez estoy tomando falopa'
-	if message.content.startswith('-me encanta'):
-		message_bot ='maldita falopa'
-		
+	if message.content.startswith('-'):
+		message_bot = 'No te entendi'
+		if message.content.startswith('-$'):
+			message_bot = get_crypto_value(message.content[2:])
+		elif  message.content.startswith('-meme'):
+			message_bot = get_meme()
+		elif  message.content.startswith('-noticia'):
+			message_bot = message.content.split('-noticia ', 1)[1]
+			message_bot = get_news(message_bot)
+		elif  message.content.startswith('-falopa'):
+			message_bot = 'Ahhhhh otra vez estoy tomando falopa'
+		elif  message.content.startswith('-me encanta'):
+			message_bot ='maldita falopa'
 	await message.channel.send(message_bot)
+
 keep_alive()
 client.run(os.getenv('BOT_TOKEN'))
